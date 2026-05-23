cc27e966c625a33e6c2f12a5b95e0f3f0cf8a58f
LucasGabrielFaustinoDaSilva <lucas.gah2000@gmail.com>
2026-05-23 15:42:39 -0300

    adicionando memoria ao agente

--- meu_primeiro_projeto/browser_search_agent.py
+++ meu_primeiro_projeto/browser_search_agent.py
@@ -1,18 +1,28 @@
 from agno.agent import Agent
 from agno.models.groq import Groq
-
+import os
+from agno.db.sqlite import SqliteDb
 from dotenv import load_dotenv
 
 load_dotenv()
 
-agent = Agent(
-    model=Groq(id="openai/gpt-oss-safeguard-20b"),
-    tools=[{"type": "browser_search"}],
-    debug_mode=True
-)
-while True:
-    agent.print_response(input("Digite sua pergunta: "))
 
-    user_input = input("Digite sair para encerrar ou pressione Enter para continuar: ")
+db = SqliteDb(db_file="agno.db")
+
+
+agent = Agent(
+    model=Groq(id="openai/gpt-oss-120b"),
+    tools=[{"type": "browser_search"}],
+    db=db,
+    update_memory_on_run=True,
+    add_memories_to_context=True
+)
+
+
+while True:
+    pergunta = input("Digite sua pergunta: ")
+    agent.print_response(pergunta)
+    
+    user_input = input("Digite 'sair' para encerrar ou pressione Enter para continuar: ")
     if user_input.lower() == "sair":
         break
