8bf4a652f16d7576e7a1f828c5d5ac0460f392af
LucasGabrielFaustinoDaSilva <lucas.gah2000@gmail.com>
2026-05-24 16:04:29 -0300

    Adicionando separaçao por usuario e tema

--- meu_primeiro_projeto/browser_search_agent.py
+++ meu_primeiro_projeto/browser_search_agent.py
@@ -11,16 +11,21 @@ db = SqliteDb(db_file="agno.db")
 
 
 agent = Agent(
+    name="comentarista_esportivo",
+    user_id="user1",
+    session_id="esportes",
     model=Groq(id="openai/gpt-oss-120b"),
     tools=[{"type": "browser_search"}],
     db=db,
     update_memory_on_run=True,
-    add_memories_to_context=True
+    add_memories_to_context=True,
+    add_history_to_context=True,
+    num_history_runs=5
 )
 
 
 while True:
-    pergunta = input("Digite sua pergunta: ")
+    pergunta = input("Digite sua pergunta sobre futebol: ")
     agent.print_response(pergunta)
     
     user_input = input("Digite 'sair' para encerrar ou pressione Enter para continuar: ")
